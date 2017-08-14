f5351677fb9c15bbc56909e41a7a9914ba8da84b
sthk <hiroka.sato@rakus.co.jp>
2017-08-14 15:22:32 +0900

    メンバー登録後、ログイン画面を表示するよう変更しました。

--- src/main/java/jp/co/rakus/stockmanagement/web/MemberController.java
+++ src/main/java/jp/co/rakus/stockmanagement/web/MemberController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,13 +51,14 @@ public class MemberController {
 	 * @param model モデル
 	 * @return ログイン画面
 	 */
-	@RequestMapping(value = "create")
-	public String create(@Validated MemberForm form, 
-			Model model) {
+	@RequestMapping(value = "/create")
+	public String create(@Validated MemberForm form, BindingResult rs,Model model) {
+		
 		Member member = new Member();
 		BeanUtils.copyProperties(form, member);
 		memberService.save(member);
-		return "book/list";
+		
+		return "redirect:/";
 	}
 	
 }
